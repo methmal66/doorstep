9eb2f4cad189c1a2d49e9132ff8c7ca23146d9f4
Sanuja Methmal <methmal66@yahoo.com>
2022-11-16 12:07:49 +0530

    route to item preivew page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,13 @@
 import React from "react";
-import logo from "../logo.svg";
-import { makeStyles } from "@material-ui/styles";
-
-const useStyles = makeStyles({
-  App: {
-    textAlign: "center",
-  },
-  AppLogo: {
-    height: "80px",
-  },
-  AppHeader: {
-    backgroundColor: "#222",
-    height: "150px",
-    padding: "20px",
-    color: "white",
-  },
-  AppIntro: {
-    fontSize: "large",
-  },
-});
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PreviewPage from "./PreviewPage";
 
 export default function App() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.App}>
-      <div className={classes.AppHeader}>
-        <img src={logo} className={classes.AppLogo} alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className={classes.AppIntro}>Hello World!</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PreviewPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
