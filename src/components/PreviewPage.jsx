b6e63a571a8f9b878e41fa4a907611d32a65cedd
Sanuja Methmal <methmal66@yahoo.com>
2022-11-16 16:53:30 +0530

    center item list

--- src/components/PreviewPage.jsx
+++ src/components/PreviewPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ItemPreviewList from "./ItemPreviewList";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 100,
+  },
+});
 
 export default function PreviewPage() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Preview page
+    <div className={classes.root}>
       <ItemPreviewList />
     </div>
   );
