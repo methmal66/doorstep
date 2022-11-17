0b7174ef8b17ec439a593e22b94070eebec76bb6
Sanuja Methmal <methmal66@yahoo.com>
2022-11-17 20:01:30 +0530

    refactor data

---
+++ src/db.js
@@ -0,0 +1,54 @@
+export const items = [
+  {
+    id: "hjfHFk59H",
+    title:
+      "MacBook Air Laptop: Apple M1 Chip, (13” , 8GB RAM, 256GB SSD Storage)A2337 / MGN93LL/A , MGN93PA/A , MGN63LL/A, MGND3LL/A",
+    imageUrl:
+      "https://appleasia.lk/wp-content/uploads/2021/07/Apple-MacBook-Air-M1-Space-Grey-Latest-Price-In-Sri-Lanka-1-min-600x600.jpeg",
+    price: 400000,
+    deliveryFee: 0,
+    condition: "New",
+    category: "Computer",
+  },
+  {
+    id: "H8L95j6t4H",
+    title: "IPhone 12 64GB - 1 Year Genxt Warranty",
+    imageUrl:
+      "https://geniusmobile.lk/wp-content/uploads/2019/11/A5EFS1912as17oispO.jpg",
+    price: 180000,
+    deliveryFee: 2000,
+    condition: "Used",
+    category: "Mobile phone",
+  },
+  {
+    id: "73HFHkfl3ild",
+    title: "Sony Wireless Noise Canceling Over-Ear Headphones - WH1000XM4",
+    imageUrl:
+      "https://www.sony.com/image/5d02da5df552836db894cead8a68f5f3?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF",
+    price: 127600,
+    deliveryFee: 500,
+    condition: "New",
+    category: "Headphone",
+  },
+  {
+    id: "jkHwyb8njfM",
+    title:
+      "IELGY Little White Sneakers Men's Summer New Korean Trend Casual Shoes Men's Wild White Student Flat Shoe",
+    imageUrl:
+      "https://static-01.daraz.lk/p/a31b3c490670ab4d311d0ff8f7e15864.jpg",
+    price: 9710,
+    deliveryFee: 0,
+    condition: "New",
+    category: "Shoes",
+  },
+  {
+    id: "qmvk2kH7kd3F",
+    title: "Yonex Astrox Light 27i Badminton Racket",
+    imageUrl:
+      "https://static-01.daraz.lk/p/754afe29f0e32b0555fbbd523e2deab6.jpg",
+    price: 21250,
+    deliveryFee: 0,
+    condition: "Used",
+    category: "Rackets",
+  },
+];
